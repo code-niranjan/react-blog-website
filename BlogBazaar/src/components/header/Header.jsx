98ea1534cc3daa5004c8f3c4578e60bd29c05f91
niranjan <niranjankadam606@gmail.com>
2024-12-03 03:36:15 +0530

    header part

---
+++ BlogBazaar/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <>
+    <div id="header">
+        <div className="header-box"></div>
+    </div>
+    </>
+  )
+}
