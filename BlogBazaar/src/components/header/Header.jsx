9eb9b3c6f1fc9209137e0f74ca8eec86742ad488
niranjan <niranjankadam606@gmail.com>
2024-12-03 06:49:35 +0530

    add packages

--- BlogBazaar/src/components/header/Header.jsx
+++ BlogBazaar/src/components/header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Header.css'
+import { Navbar } from '../navbar/Navbar'
 
 export const Header = () => {
+
+
   return (
     <>
     <div id="header">
-        <div className="header-box"></div>
+        <div className="header-box">
+          <div className="header-logo">BlogBazzar</div>
+          <Navbar/>
+        </div>
     </div>
     </>
   )
---
+++ BlogBazaar/src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <>
+        <div className="nav">
+            
+        </div>
+    </>
+  )
+}
